4a059fb7fa746e57bcdd8ab7af09f4e99bc6cc5b
Adam Kurowski <adam.kurowski@stepstone.com>
2020-08-18 20:31:03 +0200

    ,

--- docker/app/starter.py
+++ docker/app/starter.py
@@ -4,29 +4,28 @@ import time
 import signal
 from mcrcon import MCRcon
 
-exit_timer = 120
+exit_timer = 600
 
 
 def get_people_online():
     print("Getting list of players")
     with MCRcon("localhost", "ebbeh") as mcr:
         resp = mcr.command("/list")
-        print(resp)
         return int(resp.split(' ')[2])
     return 0
 
 # Tu odpalamy minecrafta , czekamy pewien czas i rozpoczynamy pętlę crontaba   
-proc = sp.Popen(["java","-Xmx1024M","-Xms1024M","-jar","./server.jar","nogui"])
+proc = sp.Popen(["java","-Xmx2048M","-Xms2048M","-jar","./server.jar","nogui"])
 print("Started Minecraft, now waiting for crontab")
 time.sleep(120)
 while True:
-    time.sleep(5)
+    time.sleep(60)
     people_online = get_people_online()
     if people_online == 0:
-        exit_timer -= 5
+        exit_timer -= 60
         print(f"No one online, decreasing timer to {exit_timer}")
     else:
-        exit_timer = 30
+        exit_timer = 600
     if exit_timer <= 0:
         proc.terminate()
         proc.wait()
